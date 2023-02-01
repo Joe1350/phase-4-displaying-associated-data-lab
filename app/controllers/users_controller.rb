80a98802c6979045de15b8bb0288e4b5fdcb5377
Joseph Martin <josephmartin1350@gmail.com>
2023-02-01 14:59:50 -0700

    commit tree 7010b8cc7adf8dc3426c2d70f9aec99a6533d4fe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find(params[:id])
+        render json: user, include: :items
+    end
+
+    private
+
+    def user_params
+        params.permit(:id)
+    end
+
 end
